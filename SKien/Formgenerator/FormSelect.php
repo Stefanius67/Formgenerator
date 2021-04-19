96a605f09257bef3f61664866fcd06dee3976915
Stefan Kientzler <s.kien@online.de>
2021-04-19 18:34:11 +0200

    docBlock changes

--- SKien/Formgenerator/FormSelect.php
+++ SKien/Formgenerator/FormSelect.php
@@ -6,12 +6,7 @@ namespace SKien\Formgenerator;
 /**
  * HTML select element.
  *
- * #### History
- * - *2020-05-12*   initial version
- * - *2021-01-07*   PHP 7.4
- *
  * @package Formgenerator
- * @version 1.1.0
  * @author Stefanius <s.kien@online.de>
  * @copyright MIT License - see the LICENSE file for details
  */
