91cda2b9dcf7033c01f0ddb42723fe1d5f9ad894
Stefan Kientzler <s.kien@online.de>
2021-04-19 18:34:31 +0200

    docBlock changes

--- SKien/Formgenerator/NullFormData.php
+++ SKien/Formgenerator/NullFormData.php
@@ -4,11 +4,7 @@ namespace SKien\Formgenerator;
 /**
  * Empty FormData used to initialize FormGenerator Data member.
  *
- * #### History
- * - *2021-01-17*   initial version
- *
  * @package Formgenerator
- * @version 1.1.0
  * @author Stefanius <s.kien@online.de>
  * @copyright MIT License - see the LICENSE file for details
  */
