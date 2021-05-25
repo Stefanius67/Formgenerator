c35f7343987190e669310fda5804c69227238544
Stefan Kientzler <s.kientzler@online.de>
2021-05-25 11:02:35 +0200

    HTML5 validation in the phpunit test

--- SKien/Test/Formgenerator/FormBaseTestCase.php
+++ SKien/Test/Formgenerator/FormBaseTestCase.php
@@ -7,7 +7,12 @@ use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
 
 /**
- * assertValidHtml from https://gist.github.com/whymarrh/5864443
+ * Base Testcase with methods to assert that
+ * - given text is valid HTML5 document
+ * - given text is valid HTML5 block
+ * - given text is plain text (doesn't contain any HTML tags)
+ *
+ * Part of the code from https://gist.github.com/whymarrh/5864443
  *
  * @author Stefanius <s.kientzler@online.de>
  * @copyright MIT License - see the LICENSE file for details
@@ -15,13 +20,11 @@ use SimpleXMLElement;
 class FormBaseTestCase extends TestCase
 {
     /**
-     * Assert that the given HTML validates
-     *
-     * Validator.nu at <http://about.validator.nu/#tos>.
-     *
-     * @param string $html The HTML to validate
+     * Assert that the given string is a valid HTML5 document.
+     * Validator.nu at <https://about.validator.nu/#tos>.
+     * @param string $strHTML The HTML to validate
      */
-    public function assertValidHtml($html)
+    public function assertValidHtml(string $strHTML) : void
     {
         // cURL
         $curl = curl_init();
@@ -32,20 +35,22 @@ class FormBaseTestCase extends TestCase
             CURLOPT_POST           => TRUE,
             CURLOPT_POSTFIELDS     => [
                 'out'     => 'xml',
-                'content' => $html,
+                'content' => $strHTML,
             ],
         ]);
-        $response = curl_exec($curl);
-        if (!$response) {
+        $strResponse = curl_exec($curl);
+        if (!$strResponse) {
             $this->markTestIncomplete('Issues checking HTML validity.');
         }
         curl_close($curl);
 
-        $response = str_replace('<code xmlns="http://www.w3.org/1999/xhtml">', '[yellow]', $response);
-        $response = str_replace('</code>', '[end]', $response);
+        // replace the code markup inside the mixed content with placeholders before XML parsing
+        // (simpleXML doesn't support mixed content...)
+        $strResponse = str_replace('<code xmlns="http://www.w3.org/1999/xhtml">', '[yellow]', $strResponse);
+        $strResponse = str_replace('</code>', '[end]', $strResponse);
 
         // Fail if errors
-        $xml = new SimpleXMLElement($response);
+        $xml = new SimpleXMLElement($strResponse);
         $nonDocumentErrors = $xml->{'non-document-error'};
         $errors = $xml->error;
         if (count($nonDocumentErrors) > 0) {
@@ -56,13 +61,8 @@ class FormBaseTestCase extends TestCase
             $message = "HTML output did not validate:";
             foreach ($errors as $error) {
                 $message .= PHP_EOL . '-> ' . $error->message;
-                /*
-                $codes = $error->message->code;
-                foreach ($codes as $code) {
-                    $message .= ' (<' . $code . '>)';
-                }
-                */
             }
+            // for terminal output just replace the placeholders with ANSI escape codes
             $message = str_replace('[yellow]', "\e[1;35m", $message);
             $message = str_replace('[end]', "\e[0m", $message);
             $this->fail($message);
@@ -72,12 +72,22 @@ class FormBaseTestCase extends TestCase
         $this->assertTrue(TRUE);
     }
 
-    public function assertValidHtmlBlock($html)
+    /**
+     * Assert that the given string is a valid HTML5 block.
+     * The HTML block is enclsed in a valid HTML5 Document definition before
+     * validation.
+     * @param string $strHTML The HTML to validate
+     */
+    public function assertValidHtmlBlock(string $strHTML) : void
     {
-        $this->assertValidHtml('<!DOCTYPE html><html><head><title>Test</title></head><body>' . $html . '</body>');
+        $this->assertValidHtml('<!DOCTYPE html><html><head><title>Test</title></head><body>' . $strHTML . '</body>');
     }
 
-    public function assertContainsNoHtmlTag($text)
+    /**
+     * Assert that the given text don't contain any HTML tags (-> contains plain text).
+     * @param string $text
+     */
+    public function assertContainsNoHtmlTag(string $text) : void
     {
         if($text != strip_tags($text)) {
             $this->fail('Text contains HTML tags.');
